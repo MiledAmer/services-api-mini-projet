148d8c7e0dea6856f5dfbff46184a1d52120813b
MiledAmer <midou.slm8@gmail.com>
2026-05-10 22:22:15 +0100

    complete the notification service

--- apps/notification-service/src/notification-service.controller.ts
+++ apps/notification-service/src/notification-service.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { NotificationServiceService } from './notification-service.service';
+import { EventPattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class NotificationServiceController {
   constructor(private readonly notificationServiceService: NotificationServiceService) {}
 
-  @Get()
-  getHello(): string {
-    return this.notificationServiceService.getHello();
+  @EventPattern('order.created')
+  handleOrderCreated(@Payload() message: any) {
+    Logger.log(`📩 Received message: ${JSON.stringify(message)}`);
   }
 }
--- apps/order-service/src/main.ts
+++ apps/order-service/src/main.ts
@@ -4,6 +4,7 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(OrderServiceModule,{
+    
     transport : Transport.TCP,
       options : {
         port : 3002
